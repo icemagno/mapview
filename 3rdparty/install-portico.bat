a17d053727c393dc7134279a5a165d6626a36259
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2016-04-29 14:18:51 -0300

    Need Tomcat 8.0+ now

--- 3rdparty/install-portico.bat
+++ 3rdparty/install-portico.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=portico-2.0.0.jar -DgroupId=org.portico -DartifactId=portico -Dversion=2.0.0 -D packaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=portico.jar -DgroupId=org.portico -DartifactId=portico -Dversion=2.1.0 -D packaging=jar -DgeneratePom=true
